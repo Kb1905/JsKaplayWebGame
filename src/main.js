ac68512fd37231829466c57c3b233fdcff7f3ca6
kbaj0002 <kbaj0002@student.monash.edu>
2025-07-29 02:15:56 +1000

    Loading in all the assets, animations, fonts and sound effects including the background music.

--- src/main.js
+++ src/main.js
@@ -1,2 +1,38 @@
 import k from "./kaplayCtx";
 
+//Loading the sprites
+k.loadSprite("chemical-bg", "graphics/chemical-bg.png");
+k.loadSprite("platforms", "graphics/platforms.png");
+k.loadSprite("sonic", "graphics/sonic.png", {
+    sliceX: 8,
+    sliceY: 2,
+    anims: {
+        run: {from: 0, to: 7, loop: true, speed: 30 },
+        jump: {from: 8, to: 15, loop: true, speed: 100 },
+    },
+});
+k.loadSprite("ring ", "graphics/ring.png", {
+    sliceX: 16,
+    sliceY: 1,
+    anims: {
+        spin: {from: 0, to: 16, loop: true, speed: 30 },
+    },
+});
+k.loadSprite("motobug", "graphics/motobug.png", {
+    sliceX: 5,
+    sliceY: 1,
+    anims: {
+        run: {from: 0, to: 4, loop: true, speed: 8 },
+    },
+});
+
+//Loading the font
+k.loadFont("mania","fonts/mania.ttf");
+
+//Loading the sound effects including the background music
+k.loadSound("destroy", "sounds/Destroy.mp3");
+k.loadSound("hurt", "sounds/Hurt.mp3");
+k.loadSound("hyper-ring", "sounds/HyperRing.mp3");
+k.loadSound("jump", "sounds/Jump.mp3");
+k.loadSound("ring", "sounds/Ring.mp3");
+k.loadSound("city", "sounds/city.mp3");
