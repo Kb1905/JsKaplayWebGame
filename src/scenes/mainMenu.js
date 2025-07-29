1e9e8f84ac17796e4ed225becc6ee7b887b5ca3f
kbaj0002 <kbaj0002@student.monash.edu>
2025-07-29 21:14:15 +1000

    Adding the main menu background which includes infinite looping

--- src/scenes/mainMenu.js
+++ src/scenes/mainMenu.js
@@ -4,6 +4,10 @@ export default function mainMenu() {
     if (!k.getData("best-score")) k.setData("best-score", 0);
     k.onButtonPress("jump", () => k.go("game"));
 
+    // This will help with infinite scrolling
     const bgPieceWidth = 1920; 
-    
+    const bgPieces = [
+        k.add([k.sprite("chemical-bg"), k.pos(0, 0), k.scale(2), k.opacity(0.8)]),
+        k.add([k.sprite("chemical-bg"), k.pos(bgPieceWidth * 2 , 0), k.scale(2), k.opacity(0.8)]),
+    ];
 }
