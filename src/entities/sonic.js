0acd7aebefcaff5b1bf313b1ce97a27e278d2161
kbaj0002 <kbaj0002@student.monash.edu>
2025-07-30 11:22:45 +1000

    Added sonic running in the main menu

--- src/entities/sonic.js
+++ src/entities/sonic.js
@@ -1,8 +1,11 @@
+import k from "../kaplayCtx";
+
 export function makeSonic(pos) {
     const sonic = k.add([
         k.sprite("sonic", {anim: "run"}),
         k.scale(4),
         k.area(), //area helps add a hitbox
-        k.anchor(""), //anchor allows to change the origin point, the deafult is usually top left
-    ])
+        k.anchor("center"), //anchor allows to change the origin point, the deafult is usually top left
+        k.pos(pos),
+    ]);
 }
